ec1fc1b859796ae65acf7ae2e09fc74dce763b67
kiler222 <pjobkiewicz@gmail.com>
2023-01-16 17:21:12 +0100

    support iOS 13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MLKitPackage",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "MLKitPackage",
